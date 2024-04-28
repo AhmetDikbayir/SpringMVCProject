a3f43a9d3ccf8e5522e770b75ac0f51aa936f1de
Your Name <ahmet.dikbayir@gmail.com>
2024-04-29 01:03:12 +0300

    something was changed

--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -2,6 +2,7 @@ package com.tpe.service;
 
 import com.tpe.domain.Student;
 import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
@@ -11,10 +12,16 @@ import java.util.List;
 @Component()
 public class StudentService implements IStudentService{
 
+    @Autowired
     private StudentRepository studentRepository;
 
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> listAllStudents() {
+        List<Student> studentList;
+
         return null;
     }
 }
